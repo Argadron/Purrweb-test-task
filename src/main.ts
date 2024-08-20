b47fe922c88846ef4a1a579e2ad6907b89e330eb
Argadron <argadron@bk.ru>
2024-08-20 17:42:11 +0500

    Chore: cookie parser dependencie

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,7 @@ import { AppModule } from './app.module';
 import { SwaggerModuleLocal } from './swagger/swagger.module';
 import { InternalExceptionsFilter } from '@filters/internal-exceptions.filter';
 import { GlobalLogger } from '@interceptors/globalLogger.interceptor';
+import * as cookieParser from 'cookie-parser'
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -16,6 +17,7 @@ async function bootstrap() {
   app.setGlobalPrefix("api")
   app.useGlobalFilters(new InternalExceptionsFilter())
   app.useGlobalInterceptors(new GlobalLogger())
+  app.use(cookieParser())
 
   SwaggerModuleLocal.forRoot(app)
 
