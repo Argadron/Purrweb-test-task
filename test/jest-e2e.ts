46e6aee6054da83c38c6d253d89fdf24a2813d10
Argadron <argadron@bk.ru>
2024-08-20 16:52:39 +0500

    Fix: E2E test

---
+++ test/jest-e2e.ts
@@ -0,0 +1,13 @@
+import { compilerOptions } from '../tsconfig.json'
+import { pathsToModuleNameMapper } from 'ts-jest'
+
+export default {
+  "moduleFileExtensions": ["js", "json", "ts"],
+  "rootDir": ".",
+  "testEnvironment": "node",
+  "testRegex": ".e2e-spec.ts$",
+  "transform": {
+    "^.+\\.(t|j)s$": "ts-jest"
+  },
+  "moduleNameMapper": pathsToModuleNameMapper(compilerOptions.paths, { prefix: process.cwd() })
+}
